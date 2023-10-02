625a53413a1d6d72f7b77371f60f71bbc6413210
elbolaestevez <elbolaestevez@gmail.com>
2023-10-02 03:32:38 -0300

    fix date

--- __tests__/services/getDaysElapsed.test.ts
+++ __tests__/services/getDaysElapsed.test.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { TaskService } from "../../src/services/task.service";
 import { ErrorResponse } from "../../src/types/error.types";
+import { TaskRepository } from "../../src/repositories/task.repository";
 dotenv.config();
 const uri = process.env.MONGO_URI || "";
 
@@ -27,7 +28,9 @@ describe("getDaysElapsed", () => {
       unknownToStringObjectId(invalidTaskIdString);
 
     try {
-      const task = (await TaskService.getDaysElapsed(invalidTaskId)) as ErrorResponse;
+      const task = (await TaskService.getDaysElapsed(
+        invalidTaskId
+      )) as ErrorResponse;
       expect(task).toBeDefined();
       expect(task.error).toContain("Id inválido");
     } catch (error: any) {
@@ -36,19 +39,26 @@ describe("getDaysElapsed", () => {
   });
 
   it("should return the correct number of days and hours elapsed", async () => {
-    const createdAt = new Date("2023-09-01T00:00:00.000Z");
+    const mockTask: any = {
+      createdAt: new Date("2023-09-01T00:00:00.000Z"),
+    };
+    jest.spyOn(TaskRepository, "findById").mockResolvedValue(mockTask);
+    const validTaskId = new mongoose.Types.ObjectId("650b62bf5dff02dce611faa0");
 
     try {
-      const currentDate = new Date("2023-09-03T00:00:00.000Z");
-      const elapsedMilliseconds: number =
-        currentDate.getTime() - createdAt.getTime();
+      const task = await TaskService.getDaysElapsed(validTaskId);
+
+      const currentDate = new Date();
+
+      const elapsedMilliseconds: number = (currentDate.getTime() -
+        mockTask.createdAt.getTime()) as any;
 
       const elapsedDays = Math.floor(elapsedMilliseconds / (1000 * 3600 * 24));
       const elapsedHours = Math.floor(
         (elapsedMilliseconds % (1000 * 3600 * 24)) / (1000 * 3600)
       );
 
-      expect(elapsedDays).toEqual(2);
+      expect(task).toEqual({ days: elapsedDays, hours: elapsedHours });
     } catch (error) {
       throw error;
     }
